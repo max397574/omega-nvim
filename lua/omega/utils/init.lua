579733ab8619efb56044ef22ef3a6a27ff4417c6
Max <max.heinzer397@gmx.ch>
2022-10-21 11:55:34 +0200

    fix(utils): fix latex preview and last_place

--- lua/omega/utils/init.lua
+++ lua/omega/utils/init.lua
@@ -36,7 +36,7 @@ end
 --- Preview latex
 function utils.LatexPreview()
     vim.cmd.write()
-    vim.cmd([[!pdflatex %; open %:t:r.pdf]])
+    vim.cmd([[silent !pdflatex %; open %:t:r.pdf]])
 end
 
 --- Convert markdown file to html and open
@@ -207,22 +207,25 @@ utils.in_mathzone = function()
     end
 end
 
+local read_buffers = {}
 --- Go to last place in file
 function utils.last_place()
-    if vim.tbl_contains(vim.api.nvim_list_bufs(), vim.api.nvim_get_current_buf()) then
-        -- check if filetype isn't one of the listed
-        if not vim.tbl_contains({ "gitcommit", "help", "packer", "toggleterm" }, vim.bo.ft) then
-            -- check if mark `"` is inside the current file (can be false if at end of file and stuff got deleted outside neovim)
-            -- if it is go to it
-            vim.cmd(
-                [[if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif]]
-            )
-            -- get cursor position
-            local cursor = vim.api.nvim_win_get_cursor(0)
-            -- if there are folds under the cursor open them
-            if vim.fn.foldclosed(cursor[1]) ~= -1 then
-                vim.cmd.normal({ "zO", bang = true })
-            end
+    if not vim.tbl_contains(vim.api.nvim_list_bufs(), vim.api.nvim_get_current_buf()) then
+        return
+    elseif vim.tbl_contains(read_buffers, vim.api.nvim_get_current_buf()) then
+        return
+    end
+    table.insert(read_buffers, vim.api.nvim_get_current_buf())
+    -- check if filetype isn't one of the listed
+    if not vim.tbl_contains({ "gitcommit", "help", "packer", "toggleterm" }, vim.bo.ft) then
+        -- check if mark `"` is inside the current file (can be false if at end of file and stuff got deleted outside neovim)
+        -- if it is go to it
+        vim.cmd([[if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif]])
+        -- get cursor position
+        local cursor = vim.api.nvim_win_get_cursor(0)
+        -- if there are folds under the cursor open them
+        if vim.fn.foldclosed(cursor[1]) ~= -1 then
+            vim.cmd.normal({ "zO", bang = true })
         end
     end
 end
