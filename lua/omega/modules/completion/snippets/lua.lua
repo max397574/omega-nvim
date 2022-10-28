c7a1b147757ecc46b6bcac90a242adaf12e06a60
Max <max.heinzer397@gmx.ch>
2022-10-28 21:58:23 +0200

    feat(snippets/lua): add buffer, window, function snippet

--- lua/omega/modules/completion/snippets/lua.lua
+++ lua/omega/modules/completion/snippets/lua.lua
@@ -15,6 +15,10 @@ local types = require("luasnip.util.types")
 local util = require("luasnip.util.util")
 local fmt = require("luasnip.extras.fmt").fmt
 local rep = require("luasnip.extras").rep
+local extras = require("luasnip.extras")
+local m = extras.m
+local l = extras.l
+local postfix = require("luasnip.extras.postfix").postfix
 
 local parse = ls.parser.parse_snippet
 local time = [[
@@ -171,5 +175,91 @@ ls.add_snippets("lua", {
         t("print("),
         i(1, "<text>"),
         t(")"),
+    }),
+    s("buf", {
+        t("local buf = vim.api.nvim_create_buf(false,true)"),
+    }),
+    s("win", {
+        c(1, {
+            t({
+                "local width = vim.api.nvim_win_get_width(0)",
+                "local height = vim.api.nvim_win_get_height(0)",
+                "",
+            }),
+            t(""),
+        }),
+        c(2, { t("local winnr ="), t("") }),
+        t("vim.api.nvim_open_win("),
+        i(3, "buf"),
+        t(", "),
+        c(4, { t("false"), t("true") }),
+        t({ ", {", "" }),
+        t("    relative = "),
+        c(5, {
+            t('"editor"'),
+            t('"win"'),
+            t('"cursor"'),
+        }),
+        t({ ",", "" }),
+        t("    width = "),
+        i(6),
+        t({ ",", "" }),
+        t("    height = "),
+        i(7),
+        t({ ",", "" }),
+        t("    style = "),
+        i(8, "minimal"),
+        t({ ",", "" }),
+        t("    border = "),
+        i(9, "rounded"),
+        t({ ",", "" }),
+        t("    row = "),
+        i(10),
+        t({ ",", "" }),
+        t("    col = "),
+        i(11),
+        t({ ",", "" }),
+        t("})"),
+    }),
+    s("fn", {
+        c(1, {
+            sn(nil, {
+                t("local function "),
+                i(1),
+                t("("),
+                i(2),
+                t({ ")", "    " }),
+                i(3),
+                t({ "", "end", "" }),
+                i(0),
+            }),
+            sn(nil, {
+                t("function("),
+                i(1),
+                t(")"),
+                i(2),
+                t(" end"),
+                i(0),
+            }),
+            sn(nil, {
+                t("function "),
+                d(1, function()
+                    local line_count = vim.api.nvim_buf_line_count(0)
+                    local line = vim.api.nvim_buf_get_lines(0, line_count - 1, line_count, false)[1]
+                    local module = line:match("return ([%w_]+)")
+                    return sn(nil, {
+                        i(1, module or ""),
+                    })
+                end),
+                t("."),
+                i(2),
+                t("("),
+                i(3),
+                t({ ")", "    " }),
+                i(4),
+                t({ "", "end", "" }),
+                i(0),
+            }),
+        }),
     }),
 })
