93b0a88538dc28eb208c0fee59ebcb2ba3d21624
Max <max.heinzer397@gmx.ch>
2022-10-30 12:54:16 +0100

    cleanup(core): no longer use autocmd when neorg is loaded

--- lua/omega/core/autocommands.lua
+++ lua/omega/core/autocommands.lua
@@ -183,15 +183,15 @@ aucmd("DiagnosticChanged", {
     desc = "Send desktop notification",
 })
 
-aucmd("User", {
-    once = true,
-    callback = function()
-        vim.api.nvim_chan_send(vim.v.stderr, "\027]99;i=1:d=0;Neorg\027\\")
-        vim.api.nvim_chan_send(vim.v.stderr, "\027]99;i=1:d=1:p=body;Finished Loading\027\\")
-    end,
-    pattern = "NeorgStarted",
-    desc = "Send desktop notification",
-})
+-- aucmd("User", {
+--     once = true,
+--     callback = function()
+--         vim.api.nvim_chan_send(vim.v.stderr, "\027]99;i=1:d=0;Neorg\027\\")
+--         vim.api.nvim_chan_send(vim.v.stderr, "\027]99;i=1:d=1:p=body;Finished Loading\027\\")
+--     end,
+--     pattern = "NeorgStarted",
+--     desc = "Send desktop notification",
+-- })
 
 aucmd("FileType", {
     callback = function()
