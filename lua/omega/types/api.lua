251e0244215cc1038412bcc8172d0844296be2f6
Max <max.heinzer397@gmx.ch>
2022-11-02 13:23:22 +0100

    feat(types/api): add more comments

--- lua/omega/types/api.lua
+++ lua/omega/types/api.lua
@@ -1,13 +1,13 @@
 ---@alias autocmd_event "BufAdd"|"BufDelete"|"BufEnter"|"BufFilePost"|"BufFilePre"|"BufHidden"|"BufLeave"|"BufModifiedSet"|"BufNew"|"BufNewFile"|"BufRead"|"BufReadCmd"|"BufUnload"|"BufWinEnter"|"BufWinLeave"|"BufWipeout"|"BufWrite"|"BufWritePre"|"BufWriteCmd"|"BufWritePost"|"ChanInfo"|"ChanOpen"|"CmdUndefined"|"CmdlineChanged"|"CmdlineEnter"|"CmdlineLeave"|"CmdwinEnter"|"CmdwinLeave"|"ColorScheme"|"ColorschemePre"|"CompleteChanged"|"CompleteDonePre"|"CompleteDone"|"CursorHold"|"CursorHoldI"|"CursorMoved"|"CursorMovedI"|"DiffUpdated"|"DirChanged"|"DirChangedPre"|"ExitPre"|"FileAppendCmd"|"FileAppendPost"|"FileAppendPre"|"FileChangedRO"|"FileChangedShell"|"FileChangedShellPost"|"FileReadCmd"|"FileReadPost"|"FileReadPre"|"FileType"|"FileWriteCmd"|"FileWritePost"|"FileWritePre"|"FilterReadPost"|"FilterReadPre"|"FilterWritePost"|"FilterWritePre"|"FocusGained"|"FocusLost"|"FuncUndefined"|"UIEnter"|"UILeave"|"InsertChange"|"InsertCharPre"|"InsertEnter"|"InsertLeavePre"|"InsertLeave"|"MenuPopup"|"ModeChanged"|"OptionSet"|"QuickFixCmdPre"|"QuickFixCmdPost"|"QuitPre"|"RemoteReply"|"SearchWrapped"|"RecordingEnter"|"RecordingLeave"|"SessionLoadPost"|"ShellCmdPost"|"Signal"|"ShellFilterPost"|"SourcePre"|"SourcePost"|"SourceCmd"|"SpellFileMissing"|"StdinReadPost"|"StdinReadPre"|"SwapExists"|"Syntax"|"TabEnter"|"TabLeave"|"TabNew"|"TabNewEntered"|"TabClosed"|"TermOpen"|"TermEnter"|"TermLeave"|"TermClose"|"TermResponse"|"TextChanged"|"TextChangedI"|"TextChangedP"|"TextYankPost"|"User"|"UserGettingBored"|"VimEnter"|"VimLeave"|"VimLeavePre"|"VimResized"|"VimResume"|"VimSuspend"|"WinClosed"|"WinEnter"|"WinLeave"|"WinNew"|"WinScrolled"
 
 ---@class autocmd_callback_parameter
----@field id number
----@field event string
----@field group number|nil
----@field match string
----@field buf number
----@field file string
----@field data any
+---@field id number The autocommand id
+---@field event string The event which triggered the autocommand
+---@field group number|nil The id of the augroup if one exists
+---@field match string The vale of the match |<amatch>|
+---@field buf number The buffer from which the autocmd was executed
+---@field file string The file in which the autocmd was executed
+---@field data any Data that was passed to `nvim_exec_autocmds()`
 
 ---@class autocmd_opts
 ---@field pattern string|table #pattern or patterns to match against |autocmd-pattern|.
