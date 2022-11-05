3fa314caa8435a6807b1073875e46db28623ec79
Max <max.heinzer397@gmx.ch>
2022-11-05 10:29:32 +0100

    feat(config): use everforest

--- lua/omega/core/config.lua
+++ lua/omega/core/config.lua
@@ -5,7 +5,7 @@ function config.load()
         use_impatient = true,
         -- "round_colored_bg"|"round_dark_bg"|"round_blended"
         statusline = "round_blended",
-        colorscheme = "tokyonight_storm",
+        colorscheme = "everforest",
         light_colorscheme = "everforest",
         --- string "float_all_borders"|"custom_bottom_no_borders"
         telescope_theme = "float_all_borders",
