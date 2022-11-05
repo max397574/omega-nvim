5bae503b607fb4249b31e10688de11904c0e2299
Max <max.heinzer397@gmx.ch>
2022-11-05 10:30:16 +0100

    feat(completion): rename neocomplete

--- lua/omega/modules/completion/neocomplete.lua
+++ lua/omega/modules/completion/neocomplete.lua
@@ -2,7 +2,7 @@
 local neocomplete = {}
 
 neocomplete.plugins = {
-    ["neo-complete.nvim"] = { "~/neovim_plugins/neo-complete.nvim/" },
+    ["neo-complete.nvim"] = { "~/neovim_plugins/neocomplete.nvim/" },
 }
 
 return neocomplete
