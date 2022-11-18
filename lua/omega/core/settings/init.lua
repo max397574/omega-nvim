cd728b171a30d384a26f96ae5f1553a26ad2d5b6
Max <max.heinzer397@gmx.ch>
2022-11-18 16:00:09 +0100

    feat(settings): move builting plugins into main file

--- lua/omega/core/settings/init.lua
+++ lua/omega/core/settings/init.lua
@@ -1,9 +1,3 @@
--- disable builtin plugins
-require("omega.core.settings.disable_builtin")
-
--- use lua filetype detection
-vim.g.do_filetype_lua = 1
-
 vim.o.splitbelow = true
 vim.o.splitright = true
 vim.o.timeoutlen = 300
@@ -18,7 +12,7 @@ vim.o.smartcase = true
 vim.o.signcolumn = "yes:3"
 vim.o.undofile = true
 vim.o.undodir = vim.fn.expand("~") .. "/.vim/undodir" -- directory to save undofiles
-vim.o.updatetime = 300 -- update CursorHold and save swap every 2000ms
+vim.o.updatetime = 250
 vim.opt.shortmess:append("c")
 vim.opt.formatoptions = vim.opt.formatoptions
     + "r" -- continue comments after return
@@ -29,7 +23,7 @@ vim.opt.formatoptions = vim.opt.formatoptions
     - "a" -- no autoformatting
     - "o" -- don't continue comments after o/O
     - "2" -- don't use indent of second line for rest of paragraph
-vim.o.cmdheight = 1 -- height of cmd line
+vim.o.cmdheight = 0 -- height of cmd line
 vim.opt.jumpoptions:append("view")
 vim.o.virtualedit = "block" -- allow visual mode to go over end of lines
 vim.o.expandtab = true -- expand tabs to spaces
@@ -69,3 +63,34 @@ vim.opt.laststatus = 3
 
 vim.opt.grepprg = "rg --vimgrep --no-heading --smart-case"
 vim.opt.grepformat:append("%f:%l:%c:%m")
+
+-- disable builtin plugins for faster startuptime
+vim.g.loaded_python3_provider = 1
+vim.g.loaded_python_provider = 1
+vim.g.loaded_node_provider = 1
+vim.g.loaded_ruby_provider = 1
+vim.g.loaded_perl_provider = 1
+vim.g.loaded_2html_plugin = 1
+vim.g.loaded_getscript = 1
+vim.g.loaded_getscriptPlugin = 1
+vim.g.loaded_gzip = 1
+vim.g.loaded_tar = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_rrhelper = 1
+vim.g.loaded_vimball = 1
+vim.g.loaded_vimballPlugin = 1
+vim.g.loaded_zip = 1
+vim.g.loaded_zipPlugin = 1
+vim.g.loaded_tutor = 1
+vim.g.loaded_rplugin = 1
+vim.g.loaded_logiPat = 1
+vim.g.loaded_netrwSettings = 1
+vim.g.loaded_netrwFileHandlers = 1
+vim.g.loaded_syntax = 1
+vim.g.loaded_synmenu = 1
+vim.g.loaded_optwin = 1
+vim.g.loaded_compiler = 1
+vim.g.loaded_bugreport = 1
+vim.g.loaded_ftplugin = 1
+vim.g.did_load_ftplugin = 1
+vim.g.did_indent_on = 1
