34b660d509fc30f69154aaa6b987335b2e65dbcb
Max <max.heinzer397@gmx.ch>
2022-11-29 13:15:18 +0100

    remove some stuff

--- lua/omega/core/omega_global.lua
+++
@@ -1,6 +0,0 @@
-_G.omega = {}
-
-omega.load_treesitter = not vim.tbl_contains({ "[packer]", "" }, vim.fn.expand("%"))
-
-omega.modules = {}
-omega.plugin_configs = {}
--- lua/omega/modules/ui/noice.lua
+++ lua/omega/modules/ui/noice.lua
@@ -1,20 +1,6 @@
 ---@type OmegaModule
 local noice = {}
 
-noice.plugins = {
-    ["noice.nvim"] = {
-        "folke/noice.nvim",
-    },
-    ["nvim-notify"] = {
-        "rcarriga/nvim-notify",
-        module = "notify",
-    },
-    ["nui.nvim"] = {
-        "MunifTanjim/nui.nvim",
-        module = { "nui" },
-    },
-}
-
 noice.configs = {
     ["noice.nvim"] = function()
         vim.o.lazyredraw = false
