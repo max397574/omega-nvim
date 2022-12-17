e929ba811a485e11f01186e23b64cbce14417f91
Max <max.heinzer397@gmx.ch>
2022-12-17 22:12:08 +0100

    cleanup init.lua

--- init.lua
+++ init.lua
@@ -2,12 +2,6 @@ vim.defer_fn(function()
     require("impatient")
 end, 0)
 
--- dofile(vim.fn.expand("~") .. "/neovim_plugins/profiler.nvim/lua/profiler.lua")
-_G.omega = {}
-
-omega.load_treesitter = not vim.tbl_contains({ "[packer]", "" }, vim.fn.expand("%"))
-
-omega.modules = {}
-omega.plugin_configs = {}
-
 require("omega.core")
+
+_G.omega = {}
