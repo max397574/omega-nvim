26a87f88de4cd53d21707dca27c7e10fe48968f6
Max <max.heinzer397@gmx.ch>
2022-12-17 22:12:57 +0100

    better telescope live grep

--- lua/omega/modules/misc/telescope/init.lua
+++ lua/omega/modules/misc/telescope/init.lua
@@ -1,4 +1,172 @@
 local tele_mod = {}
+local tele_utils = require("telescope.utils")
+local Path = require("plenary.path")
+
+local handle_entry_index = function(opts, t, k)
+    local override = ((opts or {}).entry_index or {})[k]
+    if not override then
+        return
+    end
+
+    local val, save = override(t, opts)
+    if save then
+        rawset(t, k, val)
+    end
+    return val
+end
+
+local lookup_keys = {
+    value = 1,
+    ordinal = 1,
+}
+
+-- Gets called only once to parse everything out for the vimgrep, after that looks up directly.
+local parse_with_col = function(t)
+    local _, _, filename, lnum, col, text = string.find(t.value, [[(..-):(%d+):(%d+):(.*)]])
+
+    local ok
+    ok, lnum = pcall(tonumber, lnum)
+    if not ok then
+        lnum = nil
+    end
+
+    ok, col = pcall(tonumber, col)
+    if not ok then
+        col = nil
+    end
+
+    t.filename = filename
+    t.lnum = lnum
+    t.col = col
+    t.text = text
+
+    return { filename, lnum, col, text }
+end
+
+local parse_without_col = function(t)
+    local _, _, filename, lnum, text = string.find(t.value, [[(..-):(%d+):(.*)]])
+
+    local ok
+    ok, lnum = pcall(tonumber, lnum)
+    if not ok then
+        lnum = nil
+    end
+
+    t.filename = filename
+    t.lnum = lnum
+    t.col = nil
+    t.text = text
+
+    return { filename, lnum, nil, text }
+end
+
+local parse_only_filename = function(t)
+    t.filename = t.value
+    t.lnum = nil
+    t.col = nil
+    t.text = ""
+
+    return { t.filename, nil, nil, "" }
+end
+
+local function live_grep_entry_maker()
+    local opts = {}
+
+    local mt_vimgrep_entry
+    local parse = parse_with_col
+    if opts.__matches == true then
+        parse = parse_only_filename
+    elseif opts.__inverted == true then
+        parse = parse_without_col
+    end
+
+    local disable_devicons = opts.disable_devicons
+    local only_sort_text = opts.only_sort_text
+
+    local execute_keys = {
+        path = function(t)
+            if Path:new(t.filename):is_absolute() then
+                return t.filename, false
+            else
+                return Path:new({ t.cwd, t.filename }):absolute(), false
+            end
+        end,
+
+        filename = function(t)
+            return parse(t)[1], true
+        end,
+
+        lnum = function(t)
+            return parse(t)[2], true
+        end,
+
+        col = function(t)
+            return parse(t)[3], true
+        end,
+
+        text = function(t)
+            return parse(t)[4], true
+        end,
+    }
+
+    -- For text search only, the ordinal value is actually the text.
+    if only_sort_text then
+        execute_keys.ordinal = function(t)
+            return t.text
+        end
+    end
+
+    local display_string = "%s%s%s"
+
+    mt_vimgrep_entry = {
+        cwd = vim.fn.expand(vim.loop.cwd()),
+
+        display = function(entry)
+            local display_filename = tele_utils.transform_path(opts, entry.filename)
+
+            local separator = ": "
+
+            local display, hl_group = tele_utils.transform_devicons(
+                entry.filename,
+                string.format(display_string, display_filename, separator, entry.text),
+                disable_devicons
+            )
+
+            if hl_group then
+                return display, { { { 1, 3 }, hl_group } }
+            else
+                return display
+            end
+        end,
+
+        __index = function(t, k)
+            local override = handle_entry_index(opts, t, k)
+            if override then
+                return override
+            end
+
+            local raw = rawget(mt_vimgrep_entry, k)
+            if raw then
+                return raw
+            end
+
+            local executor = rawget(execute_keys, k)
+            if executor then
+                local val, save = executor(t)
+                if save then
+                    rawset(t, k, val)
+                end
+                return val
+            end
+
+            return rawget(t, rawget(lookup_keys, k))
+        end,
+    }
+
+    return function(line)
+        return setmetatable({ line }, mt_vimgrep_entry)
+    end
+end
 
 tele_mod.api = {
     ["colorscheme_switcher"] = function()
@@ -186,7 +354,9 @@ tele_mod.api = {
             -- layout_strategy = "custom_bottom",
             results_title = "Occurrences",
             disable_coordinates = true,
+            entry_maker = live_grep_entry_maker(),
             layout_config = {
+                preview_cutoff = 90,
                 width = 0.90,
                 height = 0.5,
                 prompt_position = "top",
