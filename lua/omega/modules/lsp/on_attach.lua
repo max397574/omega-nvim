391881aa76fd7923e015596f5faacd8023b53990
Max <max.heinzer397@gmx.ch>
2022-12-27 10:33:41 +0100

    updates

--- lua/omega/modules/lsp/on_attach.lua
+++ lua/omega/modules/lsp/on_attach.lua
@@ -19,9 +19,6 @@ local function lsp_highlight_document(client, bufnr)
 end
 
 function on_attach.setup(client, bufnr)
-    -- require("omega.modules.langs.inlay_hints").setup_autocmd()
-    -- require("omega.modules.lsp.inlay_hints").setup(bufnr)
-    -- client.server_capabilities.semanticTokensProvider = nil
     if client.server_capabilities.codeActionProvider then
         require("omega.modules.lsp.available_code_action").setup(bufnr)
     end
@@ -34,12 +31,8 @@ function on_attach.setup(client, bufnr)
     vim.keymap.set("i", "<c-k>", vim.lsp.buf.signature_help, opts)
     vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
     vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-    -- vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
     vim.keymap.set("n", "<C-d>", vim.diagnostic.goto_prev, opts)
     vim.keymap.set("n", "<C-f>", vim.diagnostic.goto_next, opts)
-    -- vim.keymap.set("n", "<Leader>fs", vim.lsp.buf.formatting_sync, opts)
-    -- require("nvim-navic").setup({ depth_limit = 4 })
-    -- require("nvim-navic").attach(client, bufnr)
     lsp_highlight_document(client, bufnr)
     require("which-key").register({
         s = {
