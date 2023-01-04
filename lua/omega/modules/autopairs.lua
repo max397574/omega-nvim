e437ffd7291bcf04e3af3a429d01434c840b86e4
Max <max.heinzer397@gmx.ch>
2023-01-04 17:51:35 +0100

    cleanup: autopairs and surround configs

--- lua/omega/modules/autopairs.lua
+++ lua/omega/modules/autopairs.lua
@@ -8,14 +8,9 @@ autopairs.config = function()
     local npairs = require("nvim-autopairs")
     require("nvim-autopairs").setup({
         ignored_next_char = "",
-        -- map_c_w = true,
-        -- disable_filetype = { "norg" },
+        disable_filetype = { "norg" },
     })
     npairs.add_rule(Rule("$", "$", "tex"))
-    npairs.remove_rule("[")
-    npairs.add_rule(Rule("[", "]", "-norg"))
-    npairs.remove_rule("(")
-    npairs.add_rule(Rule("(", ")", "-norg"))
 end
 
 return autopairs
--- lua/omega/modules/surround.lua
+++ lua/omega/modules/surround.lua
@@ -8,24 +8,17 @@ local surround = {
     end,
 }
 
-surround.config = function()
-    local get_input = function(prompt)
-        local ok, result = pcall(vim.fn.input, { prompt = prompt })
-        if not ok then
-            return nil
-        end
-        return result
+local get_input = function(prompt)
+    local ok, result = pcall(vim.fn.input, { prompt = prompt })
+    if not ok then
+        return nil
     end
+    return result
+end
+
+surround.config = function()
     require("nvim-surround").setup({
         surrounds = {
-            ["("] = { add = { "( ", " )" } },
-            [")"] = { add = { "(", ")" } },
-            ["{"] = { add = { "{ ", " }" } },
-            [""] = { add = { "{", "}" } },
-            ["<"] = { add = { "< ", " >" } },
-            [">"] = { add = { "<", ">" } },
-            ["["] = { add = { "[ ", " ]" } },
-            ["]"] = { add = { "[", "]" } },
             ["\\"] = {
                 add = function()
                     local input = get_input("Enter environment name: ")
@@ -38,51 +31,6 @@ surround.config = function()
                         return { { "\\" .. input .. "{" }, { "}" } }
                     end
                 end,
-            },
-            ["i"] = {
-                add = function()
-                    local left_delimiter = get_input("Enter the left delimiter: ")
-                    if left_delimiter then
-                        local right_delimiter = get_input("Enter the right delimiter: ")
-                        if right_delimiter then
-                            return { { left_delimiter }, { right_delimiter } }
-                        end
-                    end
-                end,
-            },
-            ["f"] = {
-                add = function()
-                    local result = get_input("Enter the function name: ")
-                    if result then
-                        return { { result .. "(" }, { ")" } }
-                    end
-                end,
-                find = "[%w_]+%b()",
-                delete = "^([%w_]+%()().-(%))()$",
-                change = {
-                    target = "^([%w_]+)().-()()$",
-                    replacement = function()
-                        local result = get_input("Enter the function name: ")
-                        if result then
-                            return { { result }, { "" } }
-                        end
-                    end,
-                },
-            },
-            ["'"] = { add = { "'", "'" } },
-            ['"'] = { add = { '"', '"' } },
-            ["`"] = { add = { "`", "`" } },
-            HTML = {
-                ["t"] = "type",
-                ["T"] = "whole",
-            },
-            aliases = {
-                ["a"] = ">",
-                ["b"] = ")",
-                ["B"] = "}",
-                ["r"] = "]",
-                ["q"] = { '"', "'", "`" },
-                ["s"] = { "}", "]", ")", ">", '"', "'", "`" },
             },
         },
         highlight = {
