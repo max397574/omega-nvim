b0008956f352feba42032b24dda17bc53b4fd401
Max <max.heinzer397@gmx.ch>
2023-01-07 09:28:04 +0100

    fix(commands): fix Tmp

--- lua/omega/core/commands.lua
+++ lua/omega/core/commands.lua
@@ -23,7 +23,7 @@ local function filetypes()
 end
 
 add_cmd("Tmp", function(args)
-    require("lazy").load("nvim-lspconfig")
+    require("lazy").load({ plugins = { "nvim-lspconfig" } })
     require("omega.extras").scratch_buf(args)
 end, {
     nargs = "?",
