5abc7e1a695df57a0794d852664c6fb85b987587
Max <max.heinzer397@gmx.ch>
2023-01-09 16:25:43 +0100

    updates

--- queries/rust/highlights.scm
+++ queries/rust/highlights.scm
@@ -14,5 +14,10 @@
 (("return" @keyword) (#set! conceal ""))
 (("break" @keyword) (#set! conceal ""))
 (("!" @keyword) (#set! conceal ""))
+
 (scoped_identifier(scoped_identifier path: (scoped_identifier) @rust_path) (#set! conceal ""))
-(scoped_identifier(scoped_identifier path: (identifier) @rust_path) (#set! conceal ""))
+
+(scoped_identifier
+  (scoped_identifier
+    path: (identifier) @rust_path)
+  (#set! conceal ""))
