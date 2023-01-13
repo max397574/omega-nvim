0cf48ace124823e2f33d4d53e57ce25a392f020c
Max <max.heinzer397@gmx.ch>
2023-01-13 07:57:33 +0100

    fix(utils): fix latex with minted package

--- lua/omega/utils/init.lua
+++ lua/omega/utils/init.lua
@@ -46,7 +46,17 @@ end
 --- Preview latex
 function utils.LatexPreview()
     vim.cmd.write()
-    vim.cmd([[silent !pdflatex %; open %:t:r.pdf]])
+    local shell_escape = false
+    for _, line in ipairs(vim.api.nvim_buf_get_lines(0, 0, -1, false)) do
+        if line:find("minted") then
+            shell_escape = true
+        end
+    end
+    if shell_escape then
+        vim.cmd([[silent !pdflatex -shell-escape %; open %:t:r.pdf]])
+    else
+        vim.cmd([[silent !pdflatex %; open %:t:r.pdf]])
+    end
 end
 
 --- Convert markdown file to html and open
