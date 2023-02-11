eeedba79dae387a125f0dabeec7e8ecdb3d6225f
Max <max.heinzer397@gmx.ch>
2023-02-11 19:12:18 +0100

    feat(colors): better indent blankline highlights for onedark

--- lua/omega/colors/custom/onedark.lua
+++ lua/omega/colors/custom/onedark.lua
@@ -2,4 +2,5 @@ vim.g.terminal_color_foreground = "#abb2bf"
 local colors = require("omega.colors").get()
 return {
     ["@tag"] = { fg = colors.blue },
+    ["IndentBlanklineChar"] = { fg = colors.orange },
 }
